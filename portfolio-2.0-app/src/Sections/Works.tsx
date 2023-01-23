13a45259099a60fa26f7538ff2192e4c65e1975e
bufer99 <bufer99@gmail.com>
2023-01-23 20:34:50 +0100

    added transition to works

--- portfolio-2.0-app/src/Sections/Works.tsx
+++ portfolio-2.0-app/src/Sections/Works.tsx
@@ -34,7 +34,7 @@ const NewLabel = () => {
     )
 }
 
-const ReferenceItem = ({ children, img, githubURL, liveURL, opened, setOpened, newItem = false }:
+const ReferenceItem = ({ children, img, githubURL, liveURL, opened, setOpened, newItem = false, isInView, delay }:
     {
         children?: React.ReactNode,
         img: string,
@@ -42,11 +42,18 @@ const ReferenceItem = ({ children, img, githubURL, liveURL, opened, setOpened, n
         liveURL?: string,
         opened: string,
         setOpened: (v: string) => void,
-        newItem?: boolean
+        newItem?: boolean,
+        isInView: boolean,
+        delay: number
     }) => {
+
+        console.log(`all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) ${delay}s`);
     return (
         <Box
             position="relative"
+            opacity={isInView ? 1 : 0}
+            transform={isInView ? "none" : "translateX(-200px)"}
+            transition={`all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) ${delay*100}ms`}
         >
             {newItem && <NewLabel />}
 
@@ -111,10 +118,6 @@ export default function Works() {
                 justifyContent="center"
                 flexWrap="wrap"
                 gap={5}
-
-                opacity={isInView ? 1 : 0}
-                transform={isInView ? "none" : "translateX(-200px)"}
-                transition={"all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"}
             >
 
                 <ReferenceItem
@@ -123,6 +126,9 @@ export default function Works() {
                     img="/references/cardGame.PNG"
                     githubURL="https://github.com/bufer99/set-game"
                     liveURL="https://bufer99.github.io/set-game/"
+                    isInView={isInView}
+                    delay={0}
+
                 >
                     One of my first work in native Javascript. A very <b>basic card game</b> for University subject.
                 </ReferenceItem>
@@ -133,6 +139,8 @@ export default function Works() {
                     img="/references/tictactoe.PNG"
                     githubURL="https://github.com/bufer99/tictactoe"
                     liveURL="https://bufer99.github.io/tictactoe/"
+                    isInView={isInView}
+                    delay={1}
                 >
                     One of my first work in native Javascript. <b>Tic Tac Toe</b> game for Uni.
                 </ReferenceItem>
@@ -143,6 +151,8 @@ export default function Works() {
                     img="/references/form.PNG"
                     githubURL="https://github.com/bufer99/formComponent-react"
                     liveURL="https://bufer99.github.io/formComponent-react/"
+                    isInView={isInView}
+                    delay={2}
                 >
                     <b>Form Component</b> My First React project in University asdasdas d asd
                 </ReferenceItem>
@@ -153,6 +163,8 @@ export default function Works() {
                     img="/references/rating.PNG"
                     githubURL="https://github.com/bufer99/interactive-rating-component-react"
                     liveURL="https://bufer99.github.io/interactive-rating-component-react/"
+                    isInView={isInView}
+                    delay={3}
                 >
 
                 </ReferenceItem>
@@ -162,6 +174,8 @@ export default function Works() {
                     img="/references/portfolio1.PNG"
                     githubURL="https://github.com/bufer99/portfolio-react"
                     liveURL="https://bufer99.com/"
+                    isInView={isInView}
+                    delay={4}
                 >
                     My <b>first</b> live website with unique domain
                 </ReferenceItem>
@@ -172,6 +186,8 @@ export default function Works() {
                     githubURL="https://github.com/bufer99/MovieRatingApp-frontend"
                     liveURL="https://move-rating-app-cc7pg.ondigitalocean.app/"
                     newItem={true}
+                    isInView={isInView}
+                    delay={5}
                 >
                     <b>Movie Rating App</b>. Full stack React+Laravel api website
                 </ReferenceItem>
